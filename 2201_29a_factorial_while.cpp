4bc138f1e178355df383e33020ff4dde09ca6bf2
Paolahz1 <101817321+Paolahz1@users.noreply.github.com>
2022-03-17 13:34:18 -0500

    Calcular con while el factorial de un numero

---
+++ 2201_29a_factorial_while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+int i=1;
+int s=1;
+int f;
+int N;
+cout<<"Bienvenido a este programa que calcula el factorial de un numero dado\n";
+cout<<"Ingrese un numero: ";
+cin>>N;
+while(i!=N){
+s=s*i;
+i=i+1;
+}
+f=s*N;
+cout<<"Factorial de "<<N <<" es "<< f;
+	return 0;
+}
