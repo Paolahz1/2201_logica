7c7e2c8719a65625d9bde05fef97b174e19da50f
Paolahz1 <101817321+Paolahz1@users.noreply.github.com>
2022-04-04 22:30:50 -0500

    Factorial con for y como función

--- 2201_29c_Factorial con for.cpp
+++ 2201_29c_Factorial con for.cpp
@@ -2,21 +2,28 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	int n;
-	int f=1;
-	int i;
-	cout<<"Ingrese un numero a calcular factorial: ";
-	cin>>n;
-	
-	for(i=2;i<=n;i++){
+
+int getFact(int x){
+    int f=1;
+    int i;
+	for(i=2;i<=x;i++){
 	f=f*i;	
 	cout<<i;
-	if(i<n){
+	if(i<x){
 	cout<<"*";
 	}
-
 }
 cout<<" = "<<f;
+	return 0;
+}
+
+
+int main(int argc, char** argv) {
+	int n;
+	int c;
+	cout<<"Ingrese un numero a calcular factorial: ";
+	cin>>n;
+	getFact(n);
+
 	return 0;
 }
